f7206d729f8c9ec17efc9c0a80495c6b9b73bb2e
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-19 16:19:03 +0100

    Creare istanza giochi

---
+++ classi/giochi.php
@@ -0,0 +1,25 @@
+<?php
+    include_once __DIR__ . '/prodotto.php';
+
+    class Giochi extends Prodotto
+    {
+        public $caratteristiche;
+        public $dimensioni;
+
+        public function __construct(
+            String $percorsoImmagine,
+            String $nome,
+            Tipologia $tipologia,
+            Float $prezzo,
+
+            Float $caratteristiche,
+            String $dimensioni
+        )
+        {
+            $this -> caratteristiche = $caratteristiche;
+            $this -> dimensioni = $dimensioni;
+            parent::__construct($percorsoImmagine, $nome, $tipologia, $prezzo);
+        }
+    }
+
+?>
