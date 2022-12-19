1139cfcccc7e53a8659881aa0ca7603c5ed27d74
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-19 14:22:48 +0100

    Set "Prodotto"

--- classi/prodotto.php
+++ classi/prodotto.php
@@ -1,5 +1,24 @@
 <?php
 
-    
+    class Prodotto
+    {
+        public $percorsoImmagine;
+        public $nome;
+        public $tipologia;
+        public $prezzo;
+
+        public function __construct(
+            String $percorsoImmagine,
+            String $nome,
+            String $tipologia,
+            Float $prezzo
+        )
+        {
+            $this -> percorsoImmagine = $percorsoImmagine;
+            $this -> nome = $nome;
+            $this -> tipologia = $tipologia;
+            $this -> prezzo = $prezzo;
+        }
+    }
     
 ?>
