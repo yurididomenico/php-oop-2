22257a7c2ceec4f5f6669add853f4fd384fc0042
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-19 15:54:52 +0100

    Funzione contruct in Prodotto

--- classi/prodotto.php
+++ classi/prodotto.php
@@ -6,6 +6,16 @@
         public $nome;
         public $tipologia;
         public $prezzo;
+
+        public function __construct($percorsoImmagine, $nome, $tipologia, $prezzo)
+        {
+            $this -> percorsoImmagine = $percorsoImmagine;
+            $this -> nome = $nome;
+            $this -> tipologia = $tipologia;
+            $this -> prezzo = $prezzo;
+        }
     }
+
+
 
 ?>
